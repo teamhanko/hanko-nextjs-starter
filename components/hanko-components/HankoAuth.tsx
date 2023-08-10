7297311755262418ba45a5eeb64bea80b9893ac3
Ashutosh-Bhadauriya <ashutoshb455@gmail.com>
2023-08-10 22:12:06 +0530

    Integrate hanko with next

---
+++ components/hanko-components/HankoAuth.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useEffect, useCallback, useState } from "react";
+import { useRouter } from "next/navigation";
+import { register, Hanko } from "@teamhanko/hanko-elements";
+
+const hankoApi = "https://f4909e45-4802-49ad-8e0b-3d34f2b8ab32.hanko.io";
+
+export default function HankoAuth() {
+  const router = useRouter();
+
+  const [hanko, setHanko] = useState<Hanko>();
+
+  useEffect(() => {
+    import("@teamhanko/hanko-elements").then(({ Hanko }) =>
+      setHanko(new Hanko(hankoApi))
+    );
+  }, []);
+
+  const redirectAfterLogin = useCallback(() => {
+    // successfully logged in, redirect to a page in your application
+    router.replace("/dashboard");
+  }, [router]);
+
+  useEffect(
+    () =>
+      hanko?.onAuthFlowCompleted(() => {
+        redirectAfterLogin();
+      }),
+    [hanko, redirectAfterLogin]
+  );
+
+  useEffect(() => {
+    register(hankoApi).catch((error) => {
+      // handle error
+    });
+  }, []);
+
+  return <hanko-auth />;
+}
