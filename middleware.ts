9b5012d54523518038807344761ed4495862af67
Kyan mensen <91885904+IAm2XTr33M@users.noreply.github.com>
2025-02-05 09:39:10 +0100

    Revert "Added the JWT validation"
    This reverts commit d7d0d01bed65683d09fcc1b56a2bb405bca9f9e1.

--- middleware.ts
+++ middleware.ts
@@ -1,34 +1,33 @@
 import { NextResponse, NextRequest } from "next/server";
 
+import { jwtVerify, createRemoteJWKSet } from "jose";
+
 const hankoApiUrl = process.env.NEXT_PUBLIC_HANKO_API_URL;
 
 export async function middleware(req: NextRequest) {
 
-  const token = req.cookies.get("hanko")?.value;
+  const hanko = req.cookies.get("hanko")?.value;
+  const tenant_id = req.cookies.get("hanko")?.value;
 
-  try {
-    const response = await fetch(hankoApiUrl + '/sessions/validate', {
-      method: 'GET',
-      headers: {
-        'Cookie': `hanko=${token}` // If using cookie
-        // 'Authorization': `Bearer ${token}` // If using Authorization header
-      }
-    });
+  const options = {method: 'GET'};
 
-    if (!response.ok) {
-      throw new Error('Session validation failed');
-    }
-    
-    const data = await response.json();
-    console.log(data)
-    
-    if(!data.is_valid){
-      throw new Error('JWT is not valid');
-    }
+  fetch(hankoApiUrl + '/sessions/validate', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.log(err));
 
-  } catch (error) {
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
+
+
+  // Original verification with JWKSet's
+  // const JWKS = createRemoteJWKSet(
+  //   new URL(`${hankoApiUrl}/.well-known/jwks.json`)
+  // );
+
+  // try {
+  //   const verifiedJWT = await jwtVerify(hanko ?? "", JWKS);
+  // } catch {
+  //   return NextResponse.redirect(new URL("/login", req.url));
+  // }
 }
 export const config = {
   matcher: ["/dashboard"],
