4a92c4afb0b072fd29ca7bee5c86fc3d67f429f6
Kyan mensen <91885904+IAm2XTr33M@users.noreply.github.com>
2025-02-13 16:49:52 +0100

    Updated the styling of dashboard and information

--- src/hanko starter components/HankoStarterDashboard.tsx
+++ src/hanko starter components/HankoStarterDashboard.tsx
@@ -13,13 +13,8 @@ const HankoStarterDashboard = () => {
 
   return (
     <div className='hankoStarterDashboard'>
-      <h1>This is an example dashboard page.</h1><br />
-      <h2>Due to the authentication in our MiddleWare script you are not able to access this page without being signed in.</h2>
-      <br /><br /><br />
-      <h2>To log out or change your profile settings like ´Emails´ , ´Password´ , ´Passkeys´ , ´Security keys´ and ´Sessions´  please click the button in the top right.</h2>
-      <br /><br /><br />
+      <h1>Dashboard</h1><br />
       <h2>Here is an example of using a custom Hook we made to get user data from Hanko using the Hanko Client.</h2>
-      <br />
       <h3>Email: {email}</h3>
       <h3>Id: {id}</h3>
     </div>
