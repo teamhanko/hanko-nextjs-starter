c9b468c9b1a32cfcea346a5f2479dbdb87c478cc
Kyan mensen <91885904+IAm2XTr33M@users.noreply.github.com>
2025-03-11 10:03:04 +0100

    Update custom.d.ts

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,10 +1,13 @@
-import { HankoAuthElementProps, HankoProfileElementProps } from "@teamhanko/hanko-elements";
+import { HankoAuthElementProps, HankoProfileElementProps, HankoEventsElementProps } from "@teamhanko/hanko-elements";
 
 declare module "react" {
   namespace JSX {
     interface IntrinsicElements {
-        'hanko-auth': HankoAuthElementProps;
-        'hanko-profile': HankoProfileElementProps;
+        "hanko-auth": HankoAuthElementProps;
+        "hanko-login": HankoAuthElementProps;
+        "hanko-registration": HankoAuthElementProps;
+        "hanko-profile": HankoProfileElementProps;
+        "hanko-events": HankoEventsElementProps;
     }
   }
 }
